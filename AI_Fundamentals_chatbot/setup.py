acf5068d011c7f1dc551c8ab1062f4c30b11b5e5
Gavininarender <122263861+Gavininarender@users.noreply.github.com>
2024-02-04 01:36:33 +0530

    Add files via upload

---
+++ AI_Fundamentals_chatbot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Narender',
+    author_email= 'narenare043@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
